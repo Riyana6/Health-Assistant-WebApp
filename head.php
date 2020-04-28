bd956c57bec4765bfb50eff3b81d71ec88d9d687
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 21:06:54 +0530

    code

---
+++ head.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>My</title>
+    <style>
+    body {
+        background-color: yellow;
+        font-size: large
+    }
+
+    div {
+        position: absolute;
+        left: 20%;
+        top: 4px;
+        margin: 10px;
+        font-size: 70px
+    }
+    </style>
+</head>
+
+<body>
+
+    <div>
+        <b>YOUR HEALTH ASSISTANT</b>
+    </div>
+</body>
+
+</html>
