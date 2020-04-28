86e3712ca5ff65feb32c0eff29b5ac045e23a6cd
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 21:38:43 +0530

    code

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 <title>Frame Details</title>
 </head>
 <frameset rows="15%,*" >
-<frame src="my.php" scrolling=no >
+<frame src="head.php" scrolling=no >
 <frameset cols="30%,*" noresize="noresize" scrolling=no>
 <frame src="bar.php">
 <frame name="Frame2" src="intro.php">
