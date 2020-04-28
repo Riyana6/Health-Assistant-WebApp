284e68307206070bb25c6afeff8a7cf028eaf033
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 21:05:46 +0530

    code

--- bar.php
+++ bar.php
@@ -1,22 +1,32 @@
 <html>
+
 <head>
-<title>Menu</title>
+    <title>Menu</title>
 </head>
 <style>
-button{background-color:orange;
-		width:90%;
-		padding:40px;
-		margin:20px;
-		}
-a{text-decoration:none;
-font-size:20px}
-a:hover{color:red;}
+button {
+    background-color: orange;
+    width: 90%;
+    padding: 40px;
+    margin: 20px;
+}
+
+a {
+    text-decoration: none;
+    font-size: 20px
+}
+
+a:hover {
+    color: red;
+}
 </style>
+
 <body style="font-size:30px">
-<button  ><a href="page1.html" target="Frame2">Introduction</a></button>
-<button ><a href="page7.html" target="Frame2">Patient Profile</a></button>
-<button ><a href="page2.html" target="Frame2">Register</a></button>
-<button ><a href="page5.html" target="Frame2">Doctor Login</a></button>
+    <button><a href="intro.php" target="Frame2">Introduction</a></button>
+    <button><a href="page7.html" target="Frame2">Patient Profile</a></button>
+    <button><a href="page5.html" target="Frame2">Doctor Login</a></button>
+    <button><a href="page2.html" target="Frame2">Register</a></button>
 
 </body>
+
 </html>
