876741c3f90dc72e2a90b963380af437a5073396
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 20:35:29 +0530

    code

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>Frame Details</title>
+</head>
+<frameset rows="15%,*" >
+<frame src="my.php" scrolling=no >
+<frameset cols="30%,*" noresize="noresize" scrolling=no>
+<frame src="bar.php">
+<frame name="Frame2" src="page1.php">
+</frameset>
+<frameset>
+
+</html>
+?>
