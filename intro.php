98e83e943bd65ee5cec530f8751a71ae4ae96a23
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 20:50:21 +0530

    code

---
+++ intro.php
@@ -0,0 +1,29 @@
+<html>
+<head></head>
+<body>
+<style>
+
+h2.a{
+background-color:powderblue
+}
+
+			
+</style>
+
+<h2 class="a" style="color:red;font-size:50px" align="center" bgcolor="yellow" >WELCOME</h2>
+<hr>
+<p bgcolor="red"><b>Small Description</b></p>
+<textarea cols="180" rows="20" width="100%" style="background-color:#e6ffe6;opacity:0.8"></textarea>
+<hr>
+<form>
+<b>Patient</b>
+<input type="text"/>
+<b>Doctor</b>
+<input type="text"/>
+<b>Donator</b>
+<input type="text"/>
+</form><br></br>
+<input type="submit" value="Sign up" background-color="#4CAF50" font-size="16px" text-align="center"/>
+  
+</body>
+</html>
