91a77342a5483b2c0e03414e4cbb2986e5088f96
JMJ Riyana <jeemariyana@gmail.com>
2020-04-28 20:59:00 +0530

    code

---
+++ bar.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Menu</title>
+</head>
+<style>
+button{background-color:orange;
+		width:90%;
+		padding:40px;
+		margin:20px;
+		}
+a{text-decoration:none;
+font-size:20px}
+a:hover{color:red;}
+</style>
+<body style="font-size:30px">
+<button  ><a href="page1.html" target="Frame2">Introduction</a></button>
+<button ><a href="page2.html" target="Frame2">Register</a></button>
+<button ><a href="page3.html" target="Frame2">Details</a></button>
+<button ><a href="page4.html" target="Frame2">Patient Profile Update</a></button>
+<button ><a href="page5.html" target="Frame2">Doctor Profile Update</a></button>
+<button ><a href="page6.html" target="Frame2">Other Profile Update</a></button>
+<button ><a href="page7.html" target="Frame2">Patient Profile</a></button>
+<button ><a href="page5.html" target="Frame2">Patient Details</a></button>
+
+</body>
+</html>
--- intro.php
+++ intro.php
@@ -15,7 +15,6 @@ background-color:powderblue
 <p bgcolor="red"><b>Small Description</b></p>
 <textarea cols="140" rows="20" width="30%" style="background-color:#e6ffe6;opacity:0.8"></textarea>
 <hr>
-<input type="submit" value="Sign up" background-color="#4CAF50" font-size="16px" text-align="center"/>
   
 </body>
 </html>
