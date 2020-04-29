601f907f2121b3152565e7970c580ac826b20135
JMJ Riyana <jeemariyana@gmail.com>
2020-04-29 10:43:15 +0530

    code

---
+++ search.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Menu</title>
+</head>
+<style>
+body{
+  background-image: url('p3.jpg');
+  background-size: 700px 1200px;
+}
+img{
+     opacity:0.5;
+		}
+		h1{
+  padding-top: 50px;
+  padding-right: 30px;
+  padding-bottom: 50px;
+  padding-left: 80px;
+}
+</style>
+<h1 align="center" color="red">SEARCH</h1>
+<hr>
+<input type="text" value="Enter ID" width="70%"style="opacity: 0.5;"></br></br></br></br>
+<input type="submit" value="ok">
+</body>
+</html>
