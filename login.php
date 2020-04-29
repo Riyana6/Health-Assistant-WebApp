33d426e7cad0f702fbcefe42245c52dcfb9c906e
JMJ Riyana <jeemariyana@gmail.com>
2020-04-29 19:54:54 +0530

    code

--- login.php
+++ login.php
@@ -43,10 +43,6 @@
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i><b>&nbsp;&nbsp;&nbsp;PSW</b></label>
                                 <input type="password" name="your_pass" id="your_pass" placeholder="Password" required/>
                             </div>
-                            <div class="form-group" >
-                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
-                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
-                            </div>
                             <div class="form-group form-button">
                                 <input type="submit" name="signin" id="signin" class="form-submit" value="LOGIN"/>
                             </div>OR<br/><br/>
