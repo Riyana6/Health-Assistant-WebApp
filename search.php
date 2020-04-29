12f5ce8c3499bf056e236341e3e90b673116e030
JMJ Riyana <jeemariyana@gmail.com>
2020-04-29 10:57:35 +0530

    code

--- search.php
+++ search.php
@@ -12,14 +12,22 @@ img{
 		}
 		h1{
   padding-top: 50px;
-  padding-right: 30px;
+  padding-right: 40px;
   padding-bottom: 50px;
   padding-left: 80px;
 }
+
+.center-block {
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+}
+
 </style>
 <h1 align="center" color="red">SEARCH</h1>
 <br>
-<input type="text" value="Enter ID" width="80%" style="opacity: 0.5;"></br></br>
-<input type="submit" value="ok">
+<input class="center-block" type="text" value="Enter ID" width="100%" style="opacity: 0.5;"></br></br>
+
+<input class="center-block" type="submit" value="ok">
 </body>
 </html>
