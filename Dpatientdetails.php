85ac240a242dbc9b28aa401045a261ee48fcefc0
JMJ Riyana <jeemariyana@gmail.com>
2020-05-03 11:45:22 +0530

    code

--- Dpatientdetails.php
+++ Dpatientdetails.php
@@ -52,119 +52,13 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 
+    <frameset rows="*,*">
+        <frame src="frame_1.html">
+            <frame src="frame_2.html">
+                <
+    </frameset>
 
-    <form action="">
-
-        <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="hospital-medicine-icon-png-favpng-r1z9JHyesUSmRqNn7WL3xkb7Q.png"
-                    class="d-inline-block align-top" alt="" style="width:75px;height:90px;">
-                Patient profile
-            </a>
-            <table class="centerTable">
-                <tr>
-                    <td><input class="center-block" type="text" name="nic" placeholder="Enter patient NIC number."
-                            style="height:50px; width:300px;opacity:0.5;"></td>
-                    <td><input class="center-block" type="submit" name="submit" value="Search"
-                            style="height:25px; width:70px;"></td>
-                </tr>
-            </table>
-
-
-
-            <img src="doctor-icon-medical-icon-people-icon-black-symbol-cross-logo-circle-png-clip-art.png"
-                class="d-inline-block align-top" alt="" style="width:75px;height:90px;">
-
-
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    User_name
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="newpatient.php">New Patient</a>
-                    <a class="dropdown-item" href="#">Help&Contact</a>
-                    <a class="dropdown-item" href="login.php">LogOut</a>
-                </div>
-            </div>
-        </nav>
-    </form>
-
-
-    <form name='form' id='' action='' method='post'>
-
-
-
-        <div class="col">
-            <table cellpadding="5">
-
-                <tr>
-                    <td><label>Full Name: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><label>Age: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><label>Blood Group: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><label>Address: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><label>NIC: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><label>DOB: </label></td>
-                    <td><input type="text" class="form-control" disabled></td>
-                </tr>
-                <tr>
-                    <td><button><a href="newtest.php" target="Frame2">Add New Test</a></button></td>
-                    <td></td>
-                </tr>
-            </table>
-
-            <br>
-            Date <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' id='category' class="shadow p-3 mb-5 bg-white rounded" style="height:40px; width:200px;"
-                disabled><br>
-
-            Syptomes <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-            Causes of the disease <img src="contact_support-24px.svg" alt="" data-toggle="tooltip"
-                data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-
-            Solution <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-            Approved medication <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-
-            Advice <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-            Attachments <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
-
-            <input type='text' name='txt_category' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled><br>
-
-        </div>
-    </form>
-
-
+   
 </body>
 
 </html>
