c64751c87ff0c06bddb69c58f37a6ac88b15cb6b
JMJ Riyana <jeemariyana@gmail.com>
2020-05-05 14:20:04 +0530

    code

--- dbnewpatientreg.php
+++ dbnewpatientreg.php
@@ -56,7 +56,7 @@ if(isset($_POST['submit']))
            
                 
                 $msg1 = "";
-                $target2 = "images/".basename($attachments);
+                $target2 = "files/".basename($attachments);
                 $sql = "SELECT TABLE nicno as SELECT nic
                 FROM patientreg WHERE nic='$nic'";
 
