4f5b9e0c5bd4247cf7e41ccec4ffdc772a214680
JMJ Riyana <jeemariyana@gmail.com>
2020-05-07 19:46:35 +0530

    code

---
+++ dbdpatientdetails.php
@@ -0,0 +1,67 @@
+<?php
+
+// php code to search data in mysql database and set it in input text
+
+if(isset($_POST['submitsearch']))
+{
+    // id to search
+    $nic = $_POST['nic'];
+    
+    // connect to mysql
+    $connection = mysqli_connect('localhost','root','','healthassistant');
+    
+    // mysql search query
+    $query = "SELECT * FROM `patientreg` WHERE `nic` = $nic LIMIT 1";
+    $query = "SELECT * FROM `$nic`";
+    
+    $result = mysqli_query($connect, $query);
+    $result = mysqli_query($connect, $query);
+    
+    // if id exist 
+    // show data in inputs
+    if(mysqli_num_rows($result) > 0)
+    {
+      while ($row = mysqli_fetch_array($result))
+      {
+        $fullname =$_POST['fullname'];
+        $bloodgroup =$_POST['bloodgroup'];
+        $address	=$_POST['address'];
+        $dob =$_POST['dob'];
+        $telephone	=$_POST['telephone'];
+        $nic =$_POST['nic'];
+        $height =$_POST['height'];
+        $weight =$_POST['weight'];
+        $image = $_FILES['image']['name'];
+        $date = $_POST['date'];
+        $symptomes = $_POST['symptomes'];
+        $cause_of_the_disease =$_POST['cause_of_the_disease'];
+        $solution = $_POST['solution'];
+        $approved_medication = $_POST['approved_medication'];
+        $attachments = $_FILES['attachments']['name'];
+      }  
+    }
+    
+    // if the id not exist
+    // show a message and clear inputs
+    else {
+        echo "Undifined ID";
+            $fname = "";
+            $lname = "";
+            $age = "";
+    }
+    
+    
+    mysqli_free_result($result);
+    mysqli_close($connect);
+    
+}
+
+// in the first time inputs are empty
+else{
+    $fname = "";
+    $lname = "";
+    $age = "";
+}
+
+
+?>
