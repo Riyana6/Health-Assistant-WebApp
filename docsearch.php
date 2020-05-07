779f08b17d94ed524494be5de35ae1c143fb352d
JMJ Riyana <jeemariyana@gmail.com>
2020-05-07 13:33:51 +0530

    code

---
+++ docsearch.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+    body {
+        background-image: url(Health-Insurance-Quotes-1920x1080.jpg);
+        opacity: 90%;
+    }
+
+    #category {
+        width: 95%;
+        height: 10%;
+        margin-left: 2.5%;
+        color: rgb(164, 164, 209);
+    }
+
+    #button {
+        margin-left: 85%;
+        width: 5%;
+    }
+
+
+    h5 {
+        font-style: inherit;
+        color: rgb(255, 255, 255);
+        margin-left: 2.5%;
+    }
+
+    h3 {
+        color: rgb(255, 255, 255);
+        text-align: center;
+
+    }
+    </style>
+
+</head>
+
+<body>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+
+    <form action="">
+
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="hospital-medicine-icon-png-favpng-r1z9JHyesUSmRqNn7WL3xkb7Q.png" width="50" height="50"
+                    class="d-inline-block align-top" alt="">
+                Patient profile
+            </a>
+
+            <a href="" style="margin-left: 55%;">
+
+                <img src="doctor-icon-medical-icon-people-icon-black-symbol-cross-logo-circle-png-clip-art.png"
+                    width="100" height="100" class="d-inline-block align-top" alt="">
+            </a>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    User_name
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Profile</a>
+                    <a class="dropdown-item" href="#">Help&Contact</a>
+                    <a class="dropdown-item" href="#">LogOut</a>
+                </div>
+            </div>
+        </nav>
+    </form>
+</body>
+
+</html>
