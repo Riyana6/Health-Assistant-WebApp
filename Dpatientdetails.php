a1d87b5c819c3f6c85b2e3e1b1a3f808f751a4f9
JMJ Riyana <jeemariyana@gmail.com>
2020-05-07 20:09:54 +0530

    code

--- Dpatientdetails.php
+++ Dpatientdetails.php
@@ -1,3 +1,106 @@
+<?php
+
+// php code to search data in mysql database and set it in input text
+
+if(isset($_POST['submitsearch']))
+{
+    // id to search
+    $nic = $_POST['nic'];
+    
+    // connect to mysql
+    $connection = mysqli_connect('localhost','root','','healthassistant');
+    
+    // mysql search query
+    $query = "SELECT * FROM `patientreg` WHERE `nic` = $nic LIMIT 1";
+    $query1 = "SELECT * FROM `$nic` WHERE date=(SELECT max(date) FROM `$nic`);";
+    
+    $result = mysqli_query($connection, $query);
+    $result1 = mysqli_query($connection, $query1);
+    
+    // if id exist 
+    // show data in inputs
+    if((mysqli_num_rows($result) > 0)&&(mysqli_num_rows($result) > 0))
+    {
+      while (($row = mysqli_fetch_array($result))&&($row = mysqli_fetch_array($result)))
+      {
+        $fullname =$row['fullname'];
+        $bloodgroup =$row['bloodgroup'];
+        $address	=$row['address'];
+        $dob =$row['dob'];
+        $telephone	=$row['telephone'];
+        $nic =$row['nic'];
+        $height =$row['height'];
+        $weight =$row['weight'];
+        $image = $row['image'];
+        $date = $row['date'];
+        $symptomes = $row['symptomes'];
+        $cause_of_the_disease =$row['cause_of_the_disease'];
+        $solution = $row['solution'];
+        $approved_medication = $row['approved_medication'];
+        $attachments = $row['attachments'];
+      }  
+    }
+    
+    // if the id not exist
+    // show a message and clear inputs
+    else {
+        echo "Undifined nic";
+        $fullname ="";
+        $bloodgroup ="";
+        $address	="";
+        $dob ="";
+        $telephone	="";
+        $nic ="";
+        $height ="";
+        $weight ="";
+        $image = "";
+        $date = "";
+        $symptomes = "";
+        $cause_of_the_disease ="";
+        $solution = "";
+        $approved_medication = "";
+        $attachments = "";
+    }
+    
+    
+    mysqli_free_result($result);
+    mysqli_free_result($result1);
+    mysqli_close($connection);
+    
+}
+
+// in the first time inputs are empty
+else{
+    $fullname ="";
+        $bloodgroup ="";
+        $address	="";
+        $dob ="";
+        $telephone	="";
+        $nic ="";
+        $height ="";
+        $weight ="";
+        $image = "";
+        $date = "";
+        $symptomes = "";
+        $cause_of_the_disease ="";
+        $solution = "";
+        $approved_medication = "";
+        $attachments = "";
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
 <html>
 
 <head>
@@ -153,8 +256,7 @@
                         Date <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
 
                         <input type='text' id='category' class="shadow p-3 mb-5 bg-white rounded"
-                            style="height:40px; width:200px;" disabled name="date"
-                            value="<?php echo $date;?>"><br>
+                            style="height:40px; width:200px;" disabled name="date" value="<?php echo $date;?>"><br>
 
                         Syptomes <img src="contact_support-24px.svg" alt="" data-toggle="tooltip"
                             data-placement="right">
@@ -184,8 +286,8 @@
 
                         Advice <img src="contact_support-24px.svg" alt="" data-toggle="tooltip" data-placement="right">
 
-                        <input type='text' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled
-                            name="advice" value="<?php echo $advice;?>"><br>
+                        <input type='text' id='category' class="shadow p-3 mb-5 bg-white rounded" disabled name="advice"
+                            value="<?php echo $advice;?>"><br>
 
                         Attachments <img src="contact_support-24px.svg" alt="" data-toggle="tooltip"
                             data-placement="right">
